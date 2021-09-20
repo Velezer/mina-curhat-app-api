5061a8381e0e8b6ed54178a0c2ba16200f406627
Arief Syaifuddin <48375364+Velezer@users.noreply.github.com>
2021-09-20 19:32:27 +0700

    no io

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 require("./db")
 const app = require("./app")
 const httpServer = require("http").createServer(app);
-const io = require("socket.io")(httpServer, options);
+// const io = require("socket.io")(httpServer, options);
 
-io.on("connection", socket => {
+// io.on("connection", socket => {
     
-});
+// });
 
 const port = process.env.PORT || 3000
 httpServer.listen(port, () => {
