61ad42344c99a3dd24f8fdb7a8d1874ed3790854
Arief Syaifuddin <asvelezer@gmail.com>
2021-10-06 17:06:05 +0700

    integration test

--- src/db.js
+++ src/db.js
@@ -2,17 +2,28 @@ const mongoose = require("mongoose")
 
 require("dotenv").config()
 
-if (process.env.NODE == 'production') {
-    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(async () => {
-            console.log(`connected db`)
-        })
-        .catch(err => console.error(err))
+let DB_URI = process.env.DB_URI
+
+if (process.env.ENVIRONTMENT === 'test') {
+    DB_URI = process.env.DB_URI_TEST
 }
 
+function dbConnect() {
+    mongoose.connect(DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        // .then(async () => {
+        //     console.log(`connected db`)
+        // })
+        .catch(err => console.error(err))
+    return mongoose.connection
+
+}
+
+function dbClose() {
+    return mongoose.disconnect()
+}
 
 const Chatroom = require("./models/Chatroom")
 const Consultant = require("./models/Consultant")
 const Message = require("./models/Message")
 const Anonym = require("./models/Anonym")
-module.exports = { Chatroom, Consultant, Message, Anonym }
+module.exports = { dbConnect, dbClose, Chatroom, Consultant, Message, Anonym }
---
+++ src/tests/app.int.test.js
@@ -0,0 +1,156 @@
+process.env.ENVIRONTMENT = 'test'
+const db = require("../db")
+const bcrypt = require("bcrypt")
+const jwt = require("jwt-then")
+
+const request = require("supertest")
+const createApp = require("../app")
+
+const app = createApp(db, bcrypt, jwt)
+
+jest.setTimeout(11000)
+
+describe('handler anonym --- /api/anonym', () => {
+    beforeAll((done) => {
+        db.dbConnect()
+            .once('open', () => done())
+            .on('error', (error) => done(error))
+    })
+    afterAll((done) => {
+        db.dbClose()
+            .then(() => done())
+            .catch((err) => done(err))
+    })
+    const anonymData = { name: 'name', gender: 'male' }
+    it('POST /login --> 200 server up', async () => {
+        await request(app).post('/api/anonym/login')
+            .send(anonymData)
+            .expect('Content-Type', /json/)
+            .expect(200)
+    })
+})
+// describe('handler chatrooms --- /api/chatrooms', () => {
+//     const jwt_token = 'token'
+//     const payloadAnonym = { name: 'anonym', role: 'anonym' }
+//     const payloadConsultant = { name: 'consultant', role: 'consultant' }
+//     const chatroomData = {
+//         name: 'chatroom_name',
+//         consultant: 'consultant',
+//         anonym: 'anonym',
+//         token_chatroom: 'token_chatroom'
+//     }
+//     it('POST / --> 401 no auth header', async () => {
+//         await request(app).post('/api/chatrooms')
+//             .expect('Content-Type', /json/)
+//             .expect(401, { message: `no auth header` })
+//     })
+//     it('POST / --> 401 invalid jwt', async () => {
+//         await request(app).post('/api/chatrooms')
+//             .set('Authorization', `Bearer ${jwt_token}`)
+//             .expect('Content-Type', /json/)
+//             .expect(401)
+//     })
+//     it('POST / --> 400 no input data but valid jwt', async () => {
+//         await request(app).post('/api/chatrooms')
+//             .set('Authorization', `Bearer ${jwt_token}`)
+//             .expect('Content-Type', /json/)
+//             .expect(400)
+//     })
+//     it('POST / --> 400 role is not anonym', async () => {
+//         await request(app).post('/api/chatrooms')
+//             .set('Authorization', `Bearer ${jwt_token}`)
+//             .send(chatroomData)
+//             .expect('Content-Type', /json/)
+//             .expect(400, { message: 'only anonym can create chatroom' })
+//     })
+//     it('POST / --> 201 chatroom created', async () => {
+//         await request(app).post('/api/chatrooms')
+//             .set('Authorization', `Bearer ${jwt_token}`)
+//             .send(chatroomData)
+//             .expect('Content-Type', /json/)
+//             .expect(201)
+//     })
+//     it('POST / --> 201 chatroom created', async () => {
+//         await request(app).post('/api/chatrooms')
+//             .set('Authorization', `Bearer ${jwt_token}`)
+//             .send(chatroomData)
+//             .expect('Content-Type', /json/)
+//             .expect(500, { message: 'save error' })
+//     })
+//     it('GET / --> 200 getChatrooms anonym', async () => {
+//         await request(app).get('/api/chatrooms')
+//             .set('Authorization', `Bearer ${jwt_token}`)
+//             .expect('Content-Type', /json/)
+//             .expect(200, {
+//                 message: `getChatrooms`,
+//                 data: [] // based on mock value, i use [] for simplicity
+//             })
+//     })
+//     it('GET / --> 200 getChatrooms consultant', async () => {
+//         await request(app).get('/api/chatrooms')
+//             .set('Authorization', `Bearer ${jwt_token}`)
+//             .expect('Content-Type', /json/)
+//             .expect(200, {
+//                 message: `getChatrooms`,
+//                 data: [] // based on mock value, i use [] for simplicity
+//             })
+//     })
+//     it('GET / --> 200 getChatroomsById', async () => {
+//         await request(app).get('/api/chatrooms/92831923')
+//             .set('Authorization', `Bearer ${jwt_token}`)
+//             .expect('Content-Type', /json/)
+//             .expect(200, {
+//                 message: `getChatroomsById`,
+//                 data: []
+//             })
+//     })
+// })
+// describe('handler consultants --- /api/consultants', () => {
+//     const consultantData = {
+//         name: 'name',
+//         password: 'password',
+//         gender: 'male',
+//         role: 'consultant'
+//     }
+//     it('GET / --> 200 get all consultants', async () => {
+//         await request(app).get('/api/consultants')
+//             .expect('Content-Type', /json/)
+//             .expect(200, {
+//                 message: `get all consultants`,
+//                 data: []
+//             })
+//     })
+//     it('POST /register --> 201 create consultant', async () => {
+//         await request(app).post('/api/consultants/register')
+//             .send(consultantData)
+//             .expect('Content-Type', /json/)
+//             .expect(201)
+//     })
+//     it('POST /register --> 409 consultant already exist', async () => {
+//         await request(app).post('/api/consultants/register')
+//             .send(consultantData)
+//             .expect('Content-Type', /json/)
+//             .expect(409)
+//     })
+//     it('POST /login --> 200 consultant login', async () => {
+//         await request(app).post('/api/consultants/login')
+//             .send(consultantData)
+//             .expect('Content-Type', /json/)
+//             .expect(200, {
+//                 message: `User ${consultantData.name} logged in`,
+//                 token: 'token'
+//             })
+//     })
+//     it('POST /login --> 400 consultant password wrong', async () => {
+//         await request(app).post('/api/consultants/login')
+//             .send(consultantData)
+//             .expect('Content-Type', /json/)
+//             .expect(400)
+//     })
+//     it('POST /login --> 404 consultant not found', async () => {
+//         await request(app).post('/api/consultants/login')
+//             .send(consultantData)
+//             .expect('Content-Type', /json/)
+//             .expect(404)
+//     })
+// })
